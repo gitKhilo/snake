bd45e104f1652f054e52b76ffa591b5d3f5d8fe5
Hassan Khalil <hfk08@mail.aub.edu>
2024-08-24 14:40:28 +0300

    Add files via upload
    added a background, images for the snake, the wall, and the food
    still some issues need fixing but overall game works okay for now

--- snake.py
+++ snake.py
@@ -8,48 +8,58 @@ width, height = 900, 600
 display = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Snake Game")
 
-# Define colors
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
-yellow = (255, 255, 102)
-white = (255, 255, 255)
-dark_blue = (0, 0, 128)
-
-# Set up clock and speed
-clock = pygame.time.Clock()
-snake_speed = 10
+# Set up snake block size and other constants
 snake_block = 30  # 3x bigger
+snake_speed = 10
+num_walls = 4  # Number of walls to appear
 
-# Set up fonts
+# Load and scale images
+snake_head_img = pygame.image.load('snake_head.png')
+snake_body_img = pygame.image.load('snake_body.png')
+apple_img = pygame.image.load('apple.png')
+background_img = pygame.image.load('background.png')
+wall_img = pygame.image.load('wall.png')
+
+snake_head_img = pygame.transform.scale(snake_head_img, (snake_block, snake_block))
+snake_body_img = pygame.transform.scale(snake_body_img, (snake_block, snake_block))
+apple_img = pygame.transform.scale(apple_img, (snake_block, snake_block))
+wall_img = pygame.transform.scale(wall_img, (snake_block, snake_block))
+background_img = pygame.transform.scale(background_img, (width, height))
+
+# Set up clock and fonts
+clock = pygame.time.Clock()
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
 
-def draw_snake(snake_block, snake_list):
-    for i, x in enumerate(snake_list):
+def draw_snake(snake_list):
+    for i, pos in enumerate(snake_list):
         if i == 0:
-            # Draw snake's head
-            pygame.draw.rect(display, green, [x[0], x[1], snake_block, snake_block])
-            pygame.draw.circle(display, black, (x[0] + snake_block // 2, x[1] + snake_block // 2), snake_block // 4)
+            display.blit(snake_head_img, pos)
         else:
-            # Draw snake's body
-            pygame.draw.rect(display, green, [x[0], x[1], snake_block, snake_block])
+            display.blit(snake_body_img, pos)
 
 def draw_walls(walls):
     for wall in walls:
-        pygame.draw.rect(display, red, [wall[0], wall[1], wall[2], wall[3]])
+        display.blit(wall_img, wall)
 
 def draw_food(foodx, foody):
-    pygame.draw.circle(display, red, (foodx + snake_block // 2, foody + snake_block // 2), snake_block // 2)
+    display.blit(apple_img, (foodx, foody))
 
 def show_score(score):
-    value = score_font.render(f"Score: {score}", True, white)
+    value = score_font.render(f"Score: {score}", True, (255, 255, 255))
     display.blit(value, [0, 0])
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     display.blit(mesg, [width / 6, height / 3])
+
+def generate_random_walls(num_walls):
+    walls = []
+    for _ in range(num_walls):
+        wall_x = round(random.randrange(0, width - snake_block) / snake_block) * snake_block
+        wall_y = round(random.randrange(0, height - snake_block) / snake_block) * snake_block
+        walls.append([wall_x, wall_y])
+    return walls
 
 def gameLoop():
     game_over = False
@@ -65,21 +75,16 @@ def gameLoop():
     length_of_snake = 1
     score = 0
 
-    foodx = round(random.randrange(0, width - snake_block) / 30.0) * 30.0
-    foody = round(random.randrange(0, height - snake_block) / 30.0) * 30.0
+    foodx = round(random.randrange(0, width - snake_block) / snake_block) * snake_block
+    foody = round(random.randrange(0, height - snake_block) / snake_block) * snake_block
 
-    # Define walls (3 or 4 small walls)
-    walls = [
-        (150, 150, 300, 30),  # Horizontal wall
-        (450, 450, 30, 300),  # Vertical wall
-        (600, 100, 200, 30),  # Horizontal wall
-    ]
+    walls = generate_random_walls(num_walls)
 
     while not game_over:
 
         while game_close:
-            display.fill(dark_blue)
-            message("You Lost! Press C-Play Again or Q-Quit", red)
+            display.blit(background_img, (0, 0))
+            message("You Lost! Press C-Play Again or Q-Quit", (213, 50, 80))
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -120,9 +125,10 @@ def gameLoop():
         x1 += x1_change
         y1 += y1_change
 
-        display.fill(dark_blue)
+        # Clear the screen by re-drawing the background
+        display.blit(background_img, (0, 0))
 
-        # Draw food and walls
+        # Draw food, walls, and score
         draw_food(foodx, foody)
         draw_walls(walls)
         show_score(score)
@@ -133,15 +139,15 @@ def gameLoop():
             del snake_list[0]
 
         # Check for collision with walls or self
-        if any([x == snake_head for x in snake_list[:-1]]) or any([wall[0] <= x1 < wall[0] + wall[2] and wall[1] <= y1 < wall[1] + wall[3] for wall in walls]):
+        if any([x == snake_head for x in snake_list[:-1]]) or any([wall[0] <= x1 < wall[0] + snake_block and wall[1] <= y1 < wall[1] + snake_block for wall in walls]):
             game_close = True
 
-        draw_snake(snake_block, snake_list)
+        draw_snake(snake_list)
         pygame.display.update()
 
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, width - snake_block) / 30.0) * 30.0
-            foody = round(random.randrange(0, height - snake_block) / 30.0) * 30.0
+            foodx = round(random.randrange(0, width - snake_block) / snake_block) * snake_block
+            foody = round(random.randrange(0, height - snake_block) / snake_block) * snake_block
             length_of_snake += 1
             score += 1
 
